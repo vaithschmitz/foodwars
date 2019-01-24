a53e27b7c84cc9fba4b9116d5f5d1a20cbfcefaa
Vaith Schmitz <vaithschmitz@Vaiths-MacBook-Pro.local>
2019-01-24 17:29:10 +0000

    testing heroku deploy

--- spec/features/players_spec.rb
+++ spec/features/players_spec.rb
@@ -9,3 +9,15 @@ feature "Testing player input" do
     expect(current_path).to eq('/play')
   end
 end
+
+feature "Player Health" do 
+  scenario "Show p2 health" do
+    visit('/')
+    expect(page).to have_field(:type => 'text')
+  end
+  scenario "Expect to redirect to /names after player input" do
+    visit('/')
+    click_button('Submit')
+    expect(current_path).to eq('/play')
+  end
+end
